c50cf695b62f2dadd63aa59cce793c1d967f02f5
Dermot Kilroy <Dermot.Kilroy@gocompare.com>
2017-03-11 05:16:09 +0000

    attempting first call to api

--- app/index.js
+++ app/index.js
@@ -1,4 +1,24 @@
+let http = require('http');
+
 let clientId = process.env.CLIENTID;
 let clientSecret = process.env.CLIENT_SECRET;
+
+let options = {
+    host: 'https://slack.com/oauth/authorize', 
+    path: '?client_id=' + clientId + '&scope=bot'
+};
+
+var callback = function(response) {
+    let str = '';
+    response.on('data', function(chunk){
+        str += chunk;
+    });
+
+    response.on('end', function() {
+        console.log(str);
+    });
+};
+
+http.request(options, callback).end();
 
 console.log(clientId + ' : ' + clientSecret);
