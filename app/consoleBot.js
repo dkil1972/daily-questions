07a49409278bfd4e2f1a6f7cd4c4300197e00905
dermot <dermot.kilroy@invoko.co.uk>
2017-04-03 20:50:51 +0100

    added a consolebot to make manual testing easier

---
+++ app/consoleBot.js
@@ -0,0 +1,17 @@
+var ConsoleBot = function () {};
+
+ConsoleBot.prototype.init = function(controller) {
+    controller.spawn();
+
+    controller.hears('hello', 'message_received', function(bot, message) {
+        controller.storage.users.get(message.user, function(err, user) {
+            if (user && user.name) {
+                bot.reply(message, 'Hello ' + user.name + '!!');
+            } else {
+                bot.reply(message, 'Hello.');
+            }
+        });
+    });
+}
+
+module.exports = ConsoleBot;
