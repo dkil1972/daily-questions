0fb3fda2a3fa75ce9014c133e033fcd8c39f7278
dermot <dermot.kilroy@invoko.co.uk>
2017-04-17 06:09:49 +0100

    consuming messages and putting them in the db

--- reminder/app/postMan.js
+++ reminder/app/postMan.js
@@ -1,5 +1,27 @@
 let amqp = require('amqplib/callback_api');
 let util = require('util');
+let MongoClient = require('mongodb').MongoClient;
+let assert = require('assert');
+
+var url = 'mongodb://172.17.0.5:27017/daily-questions-reminder';
+const db = () => {
+    return {
+        insert : (message) => {
+            MongoClient.connect(url, function(err, db) {
+                assert.equal(null, err);
+                console.log("Connected correctly to server");
+
+                db.collection('inserts').insertOne(message, function(err, r) {
+                    assert.equal(null, err);
+                    assert.equal(1, r.insertedCount);
+                    db.close();
+                });
+            });
+        }
+    }
+}
+
+
 
 const postMan = () => {
     amqp.connect('amqp://172.17.0.3', (err, conn) => {
@@ -7,8 +29,9 @@ const postMan = () => {
             let q = 'dqReminderTime';
             ch.assertQueue(q, {durable:false});
             ch.consume(q, (msg) => {
-                //store these messages in the database (remember at most once message processing)dd
-                console.log('[++++] - ' + msg.content.toString());
+                //TODO (remember at most once message processing)dd
+                console.log('processing message...');
+                db().insert(JSON.parse(msg.content));
             }, {noAck:true});
 
         });
