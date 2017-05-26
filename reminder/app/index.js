c599037a0d47d7eb641ee551729571c09d163a70
dermot <dermot.kilroy@invoko.co.uk>
2017-05-26 06:18:12 +0100

    require works inside the container for reminders

--- reminder/app/index.js
+++ reminder/app/index.js
@@ -1,6 +1,6 @@
 let Botkit = require('botkit');
 let reminders = require('./reminders');
-let postMan = require('./PostMan')(reminders);
+let postMan = require('./postMan')(reminders);
 let consoleBot = require('./consoleBot')(reminders);
 let slackBot = require('./slackBot')(reminders);
 
